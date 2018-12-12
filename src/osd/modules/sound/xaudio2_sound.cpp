cscd98/hbmame-libretro

commit 81c385ac5368e95efcd701ee02b1583f7c722727

Simpler low-cost implementation making modules and function pointers class members instead (nw)

src/osd/modules/sound/xaudio2_sound.cpp
@@ -123,12 +123,6 @@ public:
 typedef std::unique_ptr<IXAudio2MasteringVoice, xaudio2_custom_deleter> mastering_voice_ptr;
 typedef std::unique_ptr<IXAudio2SourceVoice, xaudio2_custom_deleter> src_voice_ptr;
 
-// XAudio2 API
-// 
-DYNAMIC_API_BEGIN(xaudio2, "XAudio2_9.dll", "XAudio2_8.dll")
-	DYNAMIC_API_FN(HRESULT, WINAPI, XAudio2Create, IXAudio2 **, uint32_t, XAUDIO2_PROCESSOR)
-DYNAMIC_API_END(xaudio2)
-
 //============================================================
 //  Helper classes
 //============================================================
@@ -209,6 +203,8 @@ private:
 	uint32_t                           m_underflows;
 	BOOL                             m_in_underflow;
 	BOOL                             m_initialized;
+	DYNAMIC_API(xaudio2, "dwrite.dll");
+	DYNAMIC_API_FN(xaudio2, HRESULT, WINAPI, XAudio2Create, IXAudio2 **, uint32_t, XAUDIO2_PROCESSOR);
 
 public:
 	sound_xaudio2() :
@@ -268,7 +264,7 @@ private:
 
 bool sound_xaudio2::probe()
 {
-	return DYNAMIC_API_TEST(xaudio2, XAudio2Create);
+	return DYNAMIC_API_TEST(XAudio2Create);
 }
 
 //============================================================
@@ -285,14 +281,14 @@ int sound_xaudio2::init(osd_options const &options)
 	CoInitializeEx(nullptr, COINIT_MULTITHREADED);
 
 	// Make sure our XAudio2Create entrypoint is bound
-	if (!DYNAMIC_API_TEST(xaudio2, XAudio2Create))
+	if (!DYNAMIC_API_TEST(XAudio2Create))
 	{
 		osd_printf_error("Could not find XAudio2. Please try to reinstall DirectX runtime package.\n");
 		return 1;
 	}
 
 	// Create the IXAudio2 object
-	HR_GOERR(DYNAMIC_CALL(xaudio2, XAudio2Create, m_xAudio2.GetAddressOf(), 0, XAUDIO2_DEFAULT_PROCESSOR));
+	HR_GOERR(DYNAMIC_CALL(XAudio2Create, m_xAudio2.GetAddressOf(), 0, XAUDIO2_DEFAULT_PROCESSOR));
 
 	// make a format description for what we want
 	format.wBitsPerSample = 16;

src/osd/modules/font/font_dwrite.cpp
@@ -66,19 +66,6 @@ static const float POINTS_PER_DIP = (3.0f / 4.0f);
 #define HR_RET0( CALL ) HR_RET(CALL, 0)
 #define HR_RET1( CALL ) HR_RET(CALL, 1)
 
-// Dynamic APIs
-DYNAMIC_API_BEGIN(d2d1, "d2d1.dll")
-	DYNAMIC_API_FN(HRESULT, WINAPI, D2D1CreateFactory, D2D1_FACTORY_TYPE, REFIID, const D2D1_FACTORY_OPTIONS *, void **)
-DYNAMIC_API_END(d2d1)
-
-DYNAMIC_API_BEGIN(dwrite, "dwrite.dll")
-	DYNAMIC_API_FN(HRESULT, WINAPI, DWriteCreateFactory, DWRITE_FACTORY_TYPE, REFIID, IUnknown **)
-DYNAMIC_API_END(dwrite)
-
-DYNAMIC_API_BEGIN(locale, "kernel32.dll")
-	DYNAMIC_API_FN(int, WINAPI, GetUserDefaultLocaleName, LPWSTR, int)
-DYNAMIC_API_END(locale)
-
 // Debugging functions
 #ifdef DWRITE_DEBUGGING
 
@@ -93,12 +80,17 @@ HRESULT SaveBitmap(IWICBitmap* bitmap, GUID pixelFormat, const WCHAR *filename)
 	ComPtr<ID2D1Factory1> d2dfactory;
 	ComPtr<IDWriteFactory> dwriteFactory;
 	ComPtr<IWICImagingFactory> wicFactory;
+	
+	DYNAMIC_API(dwrite, "dwrite.dll");
+	DYNAMIC_API(d2d1, "d2d1.dll");
+	DYNAMIC_API_FN(dwrite, HRESULT, WINAPI, DWriteCreateFactory, DWRITE_FACTORY_TYPE, REFIID, IUnknown **);
+	DYNAMIC_API_FN(d2d1, HRESULT, WINAPI, D2D1CreateFactory, D2D1_FACTORY_TYPE, REFIID, const D2D1_FACTORY_OPTIONS *, void **);
 
-	if (!DYNAMIC_API_TEST(d2d1, D2D1CreateFactory) || !DYNAMIC_API_TEST(dwrite, DWriteCreateFactory))
+	if (!DYNAMIC_API_TEST(D2D1CreateFactory) || !DYNAMIC_API_TEST(DWriteCreateFactory))
 		return ERROR_DLL_NOT_FOUND;
 
 	// Create a Direct2D factory
-	HR_RETHR(DYNAMIC_CALL(d2d1, D2D1CreateFactory,
+	HR_RETHR(DYNAMIC_CALL(D2D1CreateFactory,
 		D2D1_FACTORY_TYPE_SINGLE_THREADED,
 		__uuidof(ID2D1Factory1),
 		nullptr,
@@ -108,7 +100,7 @@ HRESULT SaveBitmap(IWICBitmap* bitmap, GUID pixelFormat, const WCHAR *filename)
 	CoInitializeEx(nullptr, COINIT_APARTMENTTHREADED);
 
 	// Create a DirectWrite factory.
-	HR_RETHR(DYNAMIC_CALL(dwrite, DWriteCreateFactory,
+	HR_RETHR(DYNAMIC_CALL(DWriteCreateFactory,
 		DWRITE_FACTORY_TYPE_SHARED,
 		__uuidof(IDWriteFactory),
 		reinterpret_cast<IUnknown **>(dwriteFactory.GetAddressOf())));
@@ -655,6 +647,12 @@ private:
 class font_dwrite : public osd_module, public font_module
 {
 private:
+	DYNAMIC_API(dwrite, "dwrite.dll");
+	DYNAMIC_API(d2d1, "d2d1.dll");
+	DYNAMIC_API(locale, "kernel32.dll");
+	DYNAMIC_API_FN(dwrite, HRESULT, WINAPI, DWriteCreateFactory, DWRITE_FACTORY_TYPE, REFIID, IUnknown **);
+	DYNAMIC_API_FN(d2d1, HRESULT, WINAPI, D2D1CreateFactory, D2D1_FACTORY_TYPE, REFIID, const D2D1_FACTORY_OPTIONS *, void **);
+	DYNAMIC_API_FN(locale, int, WINAPI, GetUserDefaultLocaleName, LPWSTR, int);
 	ComPtr<ID2D1Factory>         m_d2dfactory;
 	ComPtr<IDWriteFactory>       m_dwriteFactory;
 	ComPtr<IWICImagingFactory>   m_wicFactory;
@@ -672,7 +670,7 @@ public:
 	virtual bool probe() override
 	{
 		// This module is available if it can load the expected API Functions
-		if (!DYNAMIC_API_TEST(d2d1, D2D1CreateFactory) || !DYNAMIC_API_TEST(dwrite, DWriteCreateFactory))
+		if (!DYNAMIC_API_TEST(D2D1CreateFactory) || !DYNAMIC_API_TEST(DWriteCreateFactory))
 			return false;
 
 		return true;
@@ -685,16 +683,16 @@ public:
 		osd_printf_verbose("FontProvider: Initializing DirectWrite\n");
 
 		// Make sure we can initialize our api functions
-		if (!DYNAMIC_API_TEST(d2d1, D2D1CreateFactory) || !DYNAMIC_API_TEST(dwrite, DWriteCreateFactory))
+		if (!DYNAMIC_API_TEST(D2D1CreateFactory) || !DYNAMIC_API_TEST(DWriteCreateFactory))
 		{
 			osd_printf_error("ERROR: FontProvider: Failed to load DirectWrite functions.\n");
 			return -1;
 		}
 
-		assert(DYNAMIC_API_TEST(locale, GetUserDefaultLocaleName));
+		assert(DYNAMIC_API_TEST(GetUserDefaultLocaleName));
 
 		// Create a Direct2D factory.
-		HR_RET1(DYNAMIC_CALL(d2d1, D2D1CreateFactory,
+		HR_RET1(DYNAMIC_CALL(D2D1CreateFactory,
 			D2D1_FACTORY_TYPE_SINGLE_THREADED,
 			__uuidof(ID2D1Factory),
 			nullptr,
@@ -704,7 +702,7 @@ public:
 		CoInitializeEx(nullptr, COINIT_APARTMENTTHREADED);
 
 		// Create a DirectWrite factory.
-		HR_RET1(DYNAMIC_CALL(dwrite, DWriteCreateFactory,
+		HR_RET1(DYNAMIC_CALL(DWriteCreateFactory,
 			DWRITE_FACTORY_TYPE_SHARED,
 			__uuidof(IDWriteFactory),
 			reinterpret_cast<IUnknown **>(m_dwriteFactory.GetAddressOf())));
@@ -793,10 +791,10 @@ private:
 
 		// Get the default locale for this user if possible.
 		// GetUserDefaultLocaleName doesn't exist on XP, so don't assume.
-		if (DYNAMIC_API_TEST(locale, GetUserDefaultLocaleName))
+		if (DYNAMIC_API_TEST(GetUserDefaultLocaleName))
 		{
 			wchar_t name_buffer[LOCALE_NAME_MAX_LENGTH];
-			int len = DYNAMIC_CALL(locale, GetUserDefaultLocaleName, name_buffer, LOCALE_NAME_MAX_LENGTH);
+			int len = DYNAMIC_CALL(GetUserDefaultLocaleName, name_buffer, LOCALE_NAME_MAX_LENGTH);
 			if (len != 0)
 				locale_name = name_buffer;
 		}

src/osd/modules/lib/osdlib.h
@@ -99,48 +99,28 @@ protected:
 
 } // namespace osd
 
-//=========================================================================================================================
-// Dynamic API helpers. Useful in creating a singleton object that can be called with DYNAMIC_CALL macro for an entire API
-// Usage for defining an API is below
+//=========================================================================================================
+// Dynamic API helpers. Useful in creating a class members that expose dynamically bound API functions.
 //
-// DYNAMIC_API_BEGIN(dxgi, "dxgi.dll")
-//   DYNAMIC_API_FN(DWORD, WINAPI, CreateDXGIFactory1, REFIID, void**)
-// DYNAMIC_API_END(dxgi)
+// DYNAMIC_API(dxgi, "dxgi.dll")
+// DYNAMIC_API_FN(dxgi, DWORD, WINAPI, CreateDXGIFactory1, REFIID, void**)
 //
-// Calling then looks like: DYNAMIC_CALL(dxgi, CreateDXGIFactory1, p1, p2, etc)
-//=========================================================================================================================
+// Calling then looks like: DYNAMIC_CALL(CreateDXGIFactory1, p1, p2, etc)
+//=========================================================================================================
 
 #if !defined(OSD_UWP)
 
-#define DYNAMIC_API_BEGIN(apiname, ...) namespace osd { namespace dynamicapi { \
-class apiname##_api { \
-private: \
-	osd::dynamic_module::ptr m_module = osd::dynamic_module::open( { __VA_ARGS__ } ); \
-	static std::unique_ptr<apiname##_api> s_instance; \
-	static std::once_flag s_once; \
-public: \
-	static apiname##_api &instance() { \
-		std::call_once( apiname##_api::s_once, [](std::unique_ptr<apiname##_api> &inst) { \
-			inst = std::make_unique<apiname##_api>(); }, std::ref(s_instance)); \
-		return *apiname##_api::s_instance.get(); }
-
-#define DYNAMIC_API_FN(ret, conv, apifunc, ...) ret(conv *m_##apifunc##_pfn)( __VA_ARGS__ ) = m_module->bind<ret(conv *)( __VA_ARGS__ )>(#apifunc);
-
-#define DYNAMIC_API_END(apiname) }; \
-std::once_flag apiname##_api::s_once; \
-std::unique_ptr<apiname##_api> apiname##_api::s_instance = nullptr; }}
-
-#define DYNAMIC_CALL(apiname, fname, ...) (*osd::dynamicapi::apiname##_api::instance().m_##fname##_pfn) ( __VA_ARGS__ )
-#define DYNAMIC_API_TEST(apiname, fname) (osd::dynamicapi::apiname##_api::instance().m_##fname##_pfn != nullptr)
+#define DYNAMIC_API(apiname, ...) osd::dynamic_module::ptr m_##apiname##module = osd::dynamic_module::open( { __VA_ARGS__ } )
+#define DYNAMIC_API_FN(apiname, ret, conv, fname, ...) ret(conv *m_##fname##_pfn)( __VA_ARGS__ ) = m_##apiname##module->bind<ret(conv *)( __VA_ARGS__ )>(#fname)
+#define DYNAMIC_CALL(fname, ...) (*m_##fname##_pfn) ( __VA_ARGS__ )
+#define DYNAMIC_API_TEST(fname) (m_##fname##_pfn != nullptr)
 
 #else
 
-#define DYNAMIC_API_BEGIN(apiname, ...)
-#define DYNAMIC_API_FN(ret, conv, apifunc, ...)
-#define DYNAMIC_API_END(apiname)
-
-#define DYNAMIC_CALL(apiname, fname, ...) fname( __VA_ARGS__ )
-#define DYNAMIC_API_TEST(apiname, fname) (true)
+#define DYNAMIC_API(apiname, ...)
+#define DYNAMIC_API_FN(apiname, ret, conv, fname, ...)
+#define DYNAMIC_CALL(fname, ...) fname( __VA_ARGS__ )
+#define DYNAMIC_API_TEST(fname) (true)
 
 #endif
 

src/osd/modules/monitor/monitor_dxgi.cpp
@@ -25,10 +25,6 @@
 #include "window.h"
 #include "windows/video.h"
 
-DYNAMIC_API_BEGIN(dxgi, "dxgi.dll")
-	DYNAMIC_API_FN(DWORD, WINAPI, CreateDXGIFactory1, REFIID, void**)
-DYNAMIC_API_END(dxgi)
-
 using namespace Microsoft::WRL;
 
 class dxgi_monitor_info : public osd_monitor_info
@@ -61,6 +57,9 @@ public:
 
 class dxgi_monitor_module : public monitor_module_base
 {
+private:
+	DYNAMIC_API(dxgi, "dxgi.dll");
+	DYNAMIC_API_FN(dxgi, DWORD, WINAPI, CreateDXGIFactory1, REFIID, void**);
 public:
 	dxgi_monitor_module()
 		: monitor_module_base(OSD_MONITOR_PROVIDER, "dxgi")
@@ -69,7 +68,7 @@ public:
 
 	bool probe() override
 	{
-		if(!DYNAMIC_API_TEST(dxgi, CreateDXGIFactory1))
+		if(!DYNAMIC_API_TEST(CreateDXGIFactory1))
 			return false;
 
 		return true;
@@ -129,7 +128,7 @@ protected:
 		ComPtr<IDXGIFactory2> factory;
 		ComPtr<IDXGIAdapter> adapter;
 
-		result = DYNAMIC_CALL(dxgi, CreateDXGIFactory1, __uuidof(IDXGIFactory2), reinterpret_cast<void**>(factory.GetAddressOf())); // m_create_factory_fn();
+		result = DYNAMIC_CALL(CreateDXGIFactory1, __uuidof(IDXGIFactory2), reinterpret_cast<void**>(factory.GetAddressOf())); // m_create_factory_fn();
 		if (result != ERROR_SUCCESS)
 		{
 			osd_printf_error("CreateDXGIFactory1 failed with error 0x%x\n", static_cast<unsigned int>(result));

commit 65b17c660841a803dd9a1e1d78d104ab78eb98f7

xaudio2: fix wrong dll, regression from 81c385ac5368e95efcd701ee02b1583f7c722727 (nw) https://mametesters.org/view.php?id=7170

src/osd/modules/sound/xaudio2_sound.cpp
@@ -196,11 +196,11 @@ private:
 	std::thread                      m_audioThread;
 	std::queue<xaudio2_buffer>       m_queue;
 	std::unique_ptr<bufferpool>      m_buffer_pool;
-	uint32_t                           m_overflows;
-	uint32_t                           m_underflows;
+	uint32_t                         m_overflows;
+	uint32_t                         m_underflows;
 	BOOL                             m_in_underflow;
 	BOOL                             m_initialized;
-	OSD_DYNAMIC_API(xaudio2, "dwrite.dll");
+	OSD_DYNAMIC_API(xaudio2, "XAudio2_9.dll", "XAudio2_8.dll");
 	OSD_DYNAMIC_API_FN(xaudio2, HRESULT, WINAPI, XAudio2Create, IXAudio2 **, uint32_t, XAUDIO2_PROCESSOR);
 
 public: