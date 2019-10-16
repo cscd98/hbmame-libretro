mamedev/mame

commit 0a7b50a0054aa8aa7218c5542f00cd4b6ec723dc

Cleaned up mustache boy vblank interrupts: Got rid of the nasty long-int hack in mustache boy in favor of the same 'vblank in+vblank out' int scheme the other similar seibu games of this era used (panicrd, darkmist). This seems much more likely to be what the actual pcb used, and works just fine. [Lord Nightmare]

src/mame/includes/mustache.h
@@ -7,7 +7,7 @@ public:
 		m_spriteram(*this, "spriteram"){ }
 
 	required_shared_ptr<UINT8> m_videoram;
-	emu_timer *m_clear_irq_timer;
+	 emu_timer *m_clear_irq_timer;
 	tilemap_t *m_bg_tilemap;
 	int m_control_byte;
 	required_shared_ptr<UINT8> m_spriteram;
@@ -16,10 +16,9 @@ public:
 	DECLARE_WRITE8_MEMBER(mustache_scroll_w);
 	DECLARE_DRIVER_INIT(mustache);
 	TILE_GET_INFO_MEMBER(get_bg_tile_info);
-	virtual void machine_start();
+	 virtual void machine_start();
 	virtual void video_start();
 	virtual void palette_init();
 	UINT32 screen_update_mustache(screen_device &screen, bitmap_ind16 &bitmap, const rectangle &cliprect);
-	INTERRUPT_GEN_MEMBER(assert_irq);
-	TIMER_CALLBACK_MEMBER(clear_irq_cb);
+	TIMER_DEVICE_CALLBACK_MEMBER(mustache_scanline);
 };

src/mame/drivers/mustache.c
@@ -157,37 +157,29 @@ static GFXDECODE_START( mustache )
 	GFXDECODE_ENTRY( "gfx2", 0, spritelayout, 0x80, 8 )
 GFXDECODE_END
 
-TIMER_CALLBACK_MEMBER(mustache_state::clear_irq_cb)
-{
-	machine().device("maincpu")->execute().set_input_line(0, CLEAR_LINE);
-}
-
-INTERRUPT_GEN_MEMBER(mustache_state::assert_irq)
-{
-	device.execute().set_input_line(0, ASSERT_LINE);
-    m_clear_irq_timer->adjust(downcast<cpu_device *>(&device)->cycles_to_attotime(14288));
-       /* Timing here is an educated GUESS, Z80 /INT must stay high so the irq
-          fires no less than TWICE per frame, else game doesn't work right.
-      6000000 / 56.747 = 105732.4616 cycles per frame, we'll call it A
-      screen size is 256x256, though less is visible.
-          lets assume we have 256 lines L and 40 'lines' (really line-times)
-          of vblank V:
-      So (A/(L+V))*V = the number of cycles spent in vblank.
-      (105732.4616 / (256+40)) * 40 = 14288.17049 z80 clocks in vblank
-       */
-}
-
 void mustache_state::machine_start()
 {
-	m_clear_irq_timer = machine().scheduler().timer_alloc(timer_expired_delegate(FUNC(mustache_state::clear_irq_cb),this));
+	// do nothing, not even sure why this is here anymore.
 }
+
+TIMER_DEVICE_CALLBACK_MEMBER(mustache_state::mustache_scanline)
+{
+	int scanline = param;
+
+	if(scanline == 240) // vblank-out irq
+		machine().device("maincpu")->execute().set_input_line_and_vector(0, HOLD_LINE,0x10); /* RST 10h */
+
+	if(scanline == 0) // vblank-in irq
+		machine().device("maincpu")->execute().set_input_line_and_vector(0, HOLD_LINE,0x08); /* RST 08h */
+}
+
 
 static MACHINE_CONFIG_START( mustache, mustache_state )
 
 	/* basic machine hardware */
 	MCFG_CPU_ADD("maincpu", Z80, CPU_CLOCK)
 	MCFG_CPU_PROGRAM_MAP(memmap)
-	MCFG_CPU_VBLANK_INT_DRIVER("screen", mustache_state,  assert_irq)
+	MCFG_TIMER_DRIVER_ADD_SCANLINE("scantimer", mustache_state, mustache_scanline, "screen", 0, 1)
 
 	MCFG_CPU_ADD(CPUTAG_T5182,Z80, T5182_CLOCK)
 	MCFG_CPU_PROGRAM_MAP(t5182_map)

cscd98/hbmame-libretro

commit 77b37af9a9ea8d72f2e345efddf830c542fc3066

mustache: game runs in IM1(no irq vector used), irq stays high during vblank, old regression from https://github.com/mamedev/mame/commit/0a7b50a0054aa8aa7218c5542f00cd4b6ec723dc (nw)

src/mame/drivers/darkmist.cpp
@@ -231,10 +231,10 @@ TIMER_DEVICE_CALLBACK_MEMBER(darkmist_state::scanline)
 	int scanline = param;
 
 	if(scanline == 240) // vblank-out irq
-		m_maincpu->set_input_line_and_vector(0, HOLD_LINE,0x10); /* Z80 - RST 10h */
+		m_maincpu->set_input_line_and_vector(0, HOLD_LINE,0xd7); /* Z80 - RST 10h */
 
 	if(scanline == 0) // vblank-in irq
-		m_maincpu->set_input_line_and_vector(0, HOLD_LINE,0x08); /* Z80 - RST 08h */
+		m_maincpu->set_input_line_and_vector(0, HOLD_LINE,0xcf); /* Z80 - RST 08h */
 }
 
 

src/mame/drivers/metlfrzr.cpp
@@ -359,11 +359,11 @@ TIMER_DEVICE_CALLBACK_MEMBER(metlfrzr_state::scanline)
 	int scanline = param;
 
 	if(scanline == 240) // vblank-out irq
-		m_maincpu->set_input_line_and_vector(0, HOLD_LINE,0x10); /* Z80 - RST 10h */
+		m_maincpu->set_input_line_and_vector(0, HOLD_LINE,0xd7); /* Z80 - RST 10h */
 
 	// TODO: check this irq.
 	if(scanline == 0) // vblank-in irq
-		m_maincpu->set_input_line_and_vector(0, HOLD_LINE,0x08); /* Z80 - RST 08h */
+		m_maincpu->set_input_line_and_vector(0, HOLD_LINE,0xcf); /* Z80 - RST 08h */
 }
 
 void metlfrzr_state::metlfrzr(machine_config &config)

src/mame/drivers/mustache.cpp
@@ -166,17 +166,6 @@ static GFXDECODE_START( gfx_mustache )
 	GFXDECODE_ENTRY( "gfx2", 0, spritelayout, 0x80, 8 )
 GFXDECODE_END
 
-TIMER_DEVICE_CALLBACK_MEMBER(mustache_state::scanline)
-{
-	int scanline = param;
-
-	if(scanline == 240) // vblank-out irq
-		m_maincpu->set_input_line_and_vector(0, HOLD_LINE,0x10); /* Z80 - RST 10h */
-
-	if(scanline == 0) // vblank-in irq
-		m_maincpu->set_input_line_and_vector(0, HOLD_LINE,0x08); /* Z80 - RST 08h */
-}
-
 
 
 void mustache_state::mustache(machine_config &config)
@@ -185,7 +174,6 @@ void mustache_state::mustache(machine_config &config)
 	Z80(config, m_maincpu, CPU_CLOCK);
 	m_maincpu->set_addrmap(AS_PROGRAM, &mustache_state::memmap);
 	m_maincpu->set_addrmap(AS_OPCODES, &mustache_state::decrypted_opcodes_map);
-	TIMER(config, "scantimer").configure_scanline(FUNC(mustache_state::scanline), "screen", 0, 1);
 
 	SEI80BU(config, "sei80bu", 0).set_device_rom_tag("maincpu");
 
@@ -194,11 +182,12 @@ void mustache_state::mustache(machine_config &config)
 	/* video hardware */
 	SCREEN(config, m_screen, SCREEN_TYPE_RASTER);
 	m_screen->set_refresh_hz(56.747);
-	m_screen->set_vblank_time(ATTOSECONDS_IN_USEC(0));
+	m_screen->set_vblank_time(ATTOSECONDS_IN_USEC(2500));
 	m_screen->set_size(32*8, 32*8);
 	m_screen->set_visarea(1*8, 31*8-1, 0, 31*8-1);
 	m_screen->set_screen_update(FUNC(mustache_state::screen_update));
 	m_screen->set_palette(m_palette);
+	m_screen->screen_vblank().set_inputline(m_maincpu, INPUT_LINE_IRQ0);
 
 	GFXDECODE(config, m_gfxdecode, m_palette, gfx_mustache);
 	PALETTE(config, m_palette, palette_device::RGB_444_PROMS, "proms", 256);

src/mame/drivers/pc8401a.cpp
@@ -57,7 +57,7 @@ void pc8401a_state::scan_keyboard()
 	if (!m_key_strobe && strobe)
 	{
 		/* trigger interrupt */
-		m_maincpu->set_input_line_and_vector(INPUT_LINE_IRQ0, ASSERT_LINE, 0x28); // Z80
+		m_maincpu->set_input_line_and_vector(INPUT_LINE_IRQ0, ASSERT_LINE, 0xef); // Z80 - RST 28h
 		logerror("INTERRUPT\n");
 	}
 

src/mame/includes/mustache.h
@@ -54,7 +54,6 @@ private:
 	uint32_t screen_update(screen_device &screen, bitmap_ind16 &bitmap, const rectangle &cliprect);
 	void draw_sprites(bitmap_ind16 &bitmap, const rectangle &cliprect );
 
-	TIMER_DEVICE_CALLBACK_MEMBER(scanline);
 	void decrypted_opcodes_map(address_map &map);
 	void memmap(address_map &map);
 };