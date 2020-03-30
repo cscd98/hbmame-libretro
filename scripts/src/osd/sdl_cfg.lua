780687eb575a01a9069ab7309f1f84e16cecdc2e
Zoltan45 <rderwis@yahoo.com>
2020-03-30 17:53:32 -0400

    cleanup libretro support

--- scripts/src/osd/sdl_cfg.lua
+++ scripts/src/osd/sdl_cfg.lua
@@ -7,20 +7,11 @@ forcedincludes {
 	MAME_DIR .. "src/osd/sdl/sdlprefix.h"
 }
 
-if _OPTIONS["USE_TAPTUN"]=="1" or _OPTIONS["USE_PCAP"]==1 then
+if SDL_NETWORK~="" and not _OPTIONS["DONT_USE_NETWORK"] then
 	defines {
 		"USE_NETWORK",
+		"OSD_NET_USE_" .. string.upper(SDL_NETWORK),
 	}
-	if _OPTIONS["USE_TAPTUN"]=="1" then
-		defines {
-			"OSD_NET_USE_TAPTUN",
-		}
-	end
-	if _OPTIONS["USE_PCAP"]=="1" then
-		defines {
-			"OSD_NET_USE_PCAP",
-		}
-	end
 end
 
 if _OPTIONS["NO_OPENGL"]~="1" and _OPTIONS["USE_DISPATCH_GL"]~="1" and _OPTIONS["MESA_INSTALL_ROOT"] then
