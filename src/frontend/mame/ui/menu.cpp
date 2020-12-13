cscd98/hbmame-libretro

commit a941526996d848c8baf36a044703c1cb76894647

C++17 string handling updates (without charconv so as not to break GCC 7) - render.cpp, rendlay.cpp, ui/ui.cpp, ui/menu.cpp: Change argument types for text processing functions from const char * to std::string_view - ui/menu.cpp: Add overloads of item_append omitting the frequently empty subtext argument - cheat.cpp: Remove some c_str() calls that became unnecessary a while ago

src/frontend/mame/ui/menu.cpp
@@ -293,21 +293,21 @@ void menu::reset(reset_options options)
 	// add an item to return
 	if (!m_parent)
 	{
-		item_append(_("Return to Machine"), "", 0, nullptr);
+		item_append(_("Return to Machine"), 0, nullptr);
 	}
 	else if (m_parent->is_special_main_menu())
 	{
 		if (machine().options().ui() == emu_options::UI_SIMPLE)
-			item_append(_("Exit"), "", 0, nullptr);
+			item_append(_("Exit"), 0, nullptr);
 		else
-			item_append(_("Exit"), "", FLAG_LEFT_ARROW | FLAG_RIGHT_ARROW, nullptr);
+			item_append(_("Exit"), FLAG_LEFT_ARROW | FLAG_RIGHT_ARROW, nullptr);
 	}
 	else
 	{
 		if (machine().options().ui() != emu_options::UI_SIMPLE && stack_has_special_main_menu())
-			item_append(_("Return to Previous Menu"), "", FLAG_LEFT_ARROW | FLAG_RIGHT_ARROW, nullptr);
+			item_append(_("Return to Previous Menu"), FLAG_LEFT_ARROW | FLAG_RIGHT_ARROW, nullptr);
 		else
-			item_append(_("Return to Previous Menu"), "", 0, nullptr);
+			item_append(_("Return to Previous Menu"), 0, nullptr);
 	}
 }
 
@@ -339,30 +339,10 @@ void menu::set_special_main_menu(bool special)
 //  end of the menu
 //-------------------------------------------------
 
-void menu::item_append(menu_item item)
-{
-	item_append(item.text, item.subtext, item.flags, item.ref, item.type);
-}
-
-//-------------------------------------------------
-//  item_append - append a new item to the
-//  end of the menu
-//-------------------------------------------------
-
 void menu::item_append(menu_item_type type, uint32_t flags)
 {
 	if (type == menu_item_type::SEPARATOR)
-		item_append(MENU_SEPARATOR_ITEM, "", flags, nullptr, menu_item_type::SEPARATOR);
-}
-
-//-------------------------------------------------
-//  item_append - append a new item to the
-//  end of the menu
-//-------------------------------------------------
-
-void menu::item_append(const std::string &text, const std::string &subtext, uint32_t flags, void *ref, menu_item_type type)
-{
-	item_append(std::string(text), std::string(subtext), flags, ref, type);
+		item_append(MENU_SEPARATOR_ITEM, flags, nullptr, menu_item_type::SEPARATOR);
 }
 
 //-------------------------------------------------
@@ -510,7 +490,7 @@ void menu::draw(uint32_t flags)
 	// first draw the FPS counter
 	if (ui().show_fps_counter())
 	{
-		ui().draw_text_full(container(), machine().video().speed_text().c_str(), 0.0f, 0.0f, 1.0f,
+		ui().draw_text_full(container(), machine().video().speed_text(), 0.0f, 0.0f, 1.0f,
 				ui::text_layout::RIGHT, ui::text_layout::WORD, mame_ui_manager::OPAQUE_, rgb_t::white(), rgb_t::black(), nullptr, nullptr);
 	}
 
@@ -533,11 +513,11 @@ void menu::draw(uint32_t flags)
 	for (auto const &pitem : m_items)
 	{
 		// compute width of left hand side
-		float total_width = gutter_width + ui().get_string_width(pitem.text.c_str()) + gutter_width;
+		float total_width = gutter_width + ui().get_string_width(pitem.text) + gutter_width;
 
 		// add in width of right hand side
 		if (!pitem.subtext.empty())
-			total_width += 2.0f * gutter_width + ui().get_string_width(pitem.subtext.c_str());
+			total_width += 2.0f * gutter_width + ui().get_string_width(pitem.subtext);
 
 		// track the maximum
 		if (total_width > visible_width)
@@ -612,7 +592,7 @@ void menu::draw(uint32_t flags)
 		{
 			auto const itemnum = top_line + linenum;
 			menu_item const &pitem = m_items[itemnum];
-			char const *const itemtext = pitem.text.c_str();
+			std::string_view const itemtext = pitem.text;
 			rgb_t fgcolor = ui().colors().text_color();
 			rgb_t bgcolor = ui().colors().text_bg_color();
 			rgb_t fgcolor2 = ui().colors().subitem_color();
@@ -689,7 +669,6 @@ void menu::draw(uint32_t flags)
 			{
 				// otherwise, draw the item on the left and the subitem text on the right
 				bool const subitem_invert(pitem.flags & FLAG_INVERT);
-				char const *subitem_text(pitem.subtext.c_str());
 				float item_width, subitem_width;
 
 				// draw the left-side text
@@ -698,7 +677,7 @@ void menu::draw(uint32_t flags)
 
 				if (pitem.flags & FLAG_COLOR_BOX)
 				{
-					rgb_t color = rgb_t((uint32_t)strtoul(subitem_text, nullptr, 16));
+					rgb_t color = rgb_t((uint32_t)strtoul(pitem.subtext.c_str(), nullptr, 16));
 
 					// give 2 spaces worth of padding
 					subitem_width = ui().get_string_width("FF00FF00");
@@ -708,6 +687,8 @@ void menu::draw(uint32_t flags)
 				}
 				else
 				{
+					std::string_view subitem_text(pitem.subtext);
+
 					// give 2 spaces worth of padding
 					item_width += 2.0f * gutter_width;
 
@@ -720,13 +701,13 @@ void menu::draw(uint32_t flags)
 					}
 
 					// customize subitem text color
-					if (!core_stricmp(subitem_text, _("On")))
+					if (!core_strnicmp(&subitem_text[0], _("On"), subitem_text.length()))
 						fgcolor2 = rgb_t(0x00,0xff,0x00);
 
-					if (!core_stricmp(subitem_text, _("Off")))
+					if (!core_strnicmp(&subitem_text[0], _("Off"), subitem_text.length()))
 						fgcolor2 = rgb_t(0xff,0x00,0x00);
 
-					if (!core_stricmp(subitem_text, _("Auto")))
+					if (!core_strnicmp(&subitem_text[0], _("Auto"), subitem_text.length()))
 						fgcolor2 = rgb_t(0xff,0xff,0x00);
 
 					// draw the subitem right-justified
@@ -769,7 +750,7 @@ void menu::draw(uint32_t flags)
 		float target_width, target_height;
 
 		// compute the multi-line target width/height
-		ui().draw_text_full(container(), pitem.subtext.c_str(), 0, 0, visible_width * 0.75f,
+		ui().draw_text_full(container(), pitem.subtext, 0, 0, visible_width * 0.75f,
 			ui::text_layout::RIGHT, ui::text_layout::WORD, mame_ui_manager::NONE, rgb_t::white(), rgb_t::black(), &target_width, &target_height);
 
 		// determine the target location
@@ -785,7 +766,7 @@ void menu::draw(uint32_t flags)
 				target_y + target_height + ui().box_tb_border(),
 				subitem_invert ? ui().colors().selected_bg_color() : ui().colors().background_color());
 
-		ui().draw_text_full(container(), pitem.subtext.c_str(), target_x, target_y, target_width,
+		ui().draw_text_full(container(), pitem.subtext, target_x, target_y, target_width,
 				ui::text_layout::RIGHT, ui::text_layout::WORD, mame_ui_manager::NORMAL, ui().colors().selected_color(), ui().colors().selected_bg_color(), nullptr, nullptr);
 	}
 
@@ -805,8 +786,8 @@ void menu::custom_render(void *selectedref, float top, float bottom, float x, fl
 
 void menu::draw_text_box()
 {
-	const char *text = m_items[0].text.c_str();
-	const char *backtext = m_items[1].text.c_str();
+	std::string_view const text = m_items[0].text;
+	std::string_view const backtext = m_items[1].text;
 	float const aspect = machine().render().ui_aspect(&container());
 	float line_height = ui().get_line_height();
 	float lr_arrow_width = 0.4f * line_height * aspect;
@@ -1286,7 +1267,7 @@ void menu::draw_arrow(float x0, float y0, float x1, float y1, rgb_t fgcolor, uin
 //  or footer text
 //-------------------------------------------------
 
-void menu::extra_text_draw_box(float origx1, float origx2, float origy, float yspan, const char *text, int direction)
+void menu::extra_text_draw_box(float origx1, float origx2, float origy, float yspan, std::string_view text, int direction)
 {
 	// get the size of the text
 	auto layout = ui().create_layout(container());
@@ -1343,14 +1324,11 @@ void menu::extra_text_position(float origx1, float origx2, float origy, float ys
 //  and footer text
 //-------------------------------------------------
 
-void menu::extra_text_render(float top, float bottom, float origx1, float origy1, float origx2, float origy2, const char *header, const char *footer)
+void menu::extra_text_render(float top, float bottom, float origx1, float origy1, float origx2, float origy2, std::string_view header, std::string_view footer)
 {
-	header = (header && *header) ? header : nullptr;
-	footer = (footer && *footer) ? footer : nullptr;
-
-	if (header != nullptr)
+	if (!header.empty())
 		extra_text_draw_box(origx1, origx2, origy1, top, header, -1);
-	if (footer != nullptr)
+	if (!footer.empty())
 		extra_text_draw_box(origx1, origx2, origy2, bottom, footer, +1);
 }
 

src/emu/render.h
@@ -56,6 +56,7 @@
 #include <memory>
 #include <mutex>
 #include <string>
+#include <string_view>
 #include <tuple>
 #include <unordered_map>
 #include <utility>
@@ -591,7 +592,7 @@ private:
 		virtual void draw_aligned(running_machine &machine, bitmap_argb32 &dest, const rectangle &bounds, int state);
 
 		// drawing helpers
-		void draw_text(render_font &font, bitmap_argb32 &dest, const rectangle &bounds, const char *str, int align, const render_color &color);
+		void draw_text(render_font &font, bitmap_argb32 &dest, const rectangle &bounds, std::string_view str, int align, const render_color &color);
 		void draw_segment_horizontal_caps(bitmap_argb32 &dest, int minx, int maxx, int midy, int width, int caps, rgb_t color);
 		void draw_segment_horizontal(bitmap_argb32 &dest, int minx, int maxx, int midy, int width, rgb_t color);
 		void draw_segment_vertical_caps(bitmap_argb32 &dest, int miny, int maxy, int midx, int width, int caps, rgb_t color);

src/emu/rendfont.cpp
@@ -856,21 +856,19 @@ float render_font::char_width(float height, float aspect, char32_t ch)
 //  at the given height
 //-------------------------------------------------
 
-float render_font::string_width(float height, float aspect, const char *string)
+float render_font::string_width(float height, float aspect, std::string_view string)
 {
 	// loop over the string and accumulate widths
 	int totwidth = 0;
 
-	const char *ends = string + strlen(string);
-	const char *s = string;
 	char32_t schar;
 
 	// loop over characters
-	while (*s != 0)
+	while (!string.empty())
 	{
-		int scharcount = uchar_from_utf8(&schar, s, ends - s);
+		int scharcount = uchar_from_utf8(&schar, &string[0], string.length());
 		totwidth += get_char(schar).width;
-		s += scharcount;
+		string.remove_prefix(scharcount);
 	}
 
 
@@ -884,21 +882,19 @@ float render_font::string_width(float height, float aspect, const char *string)
 //  UTF8-encoded string at the given height
 //-------------------------------------------------
 
-float render_font::utf8string_width(float height, float aspect, const char *utf8string)
+float render_font::utf8string_width(float height, float aspect, std::string_view utf8string)
 {
-	std::size_t const length = std::strlen(utf8string);
-
 	// loop over the string and accumulate widths
-	int count;
 	s32 totwidth = 0;
-	for (std::size_t offset = 0U; offset < length; offset += unsigned(count))
+	while (!utf8string.empty())
 	{
 		char32_t uchar;
-		count = uchar_from_utf8(&uchar, utf8string + offset, length - offset);
+		int count = uchar_from_utf8(&uchar, &utf8string[0], utf8string.length());
 		if (count < 0)
 			break;
 
 		totwidth += get_char(uchar).width;
+		utf8string.remove_prefix(count);
 	}
 
 	// scale the final result based on height

src/emu/rendfont.h
@@ -37,8 +37,8 @@ public:
 	// size queries
 	s32 pixel_height() const { return m_height; }
 	float char_width(float height, float aspect, char32_t ch);
-	float string_width(float height, float aspect, const char *string);
-	float utf8string_width(float height, float aspect, const char *utf8string);
+	float string_width(float height, float aspect, std::string_view string);
+	float utf8string_width(float height, float aspect, std::string_view utf8string);
 
 	// texture/bitmap queries
 	render_texture *get_char_texture_and_bounds(float height, float aspect, char32_t ch, render_bounds &bounds);

src/emu/rendlay.cpp
@@ -2295,7 +2295,7 @@ protected:
 	virtual void draw_aligned(running_machine &machine, bitmap_argb32 &dest, const rectangle &bounds, int state) override
 	{
 		auto font = machine.render().font_alloc("default");
-		draw_text(*font, dest, bounds, m_string.c_str(), m_textalign, color(state));
+		draw_text(*font, dest, bounds, m_string, m_textalign, color(state));
 	}
 
 private:
@@ -2985,7 +2985,7 @@ protected:
 	virtual void draw_aligned(running_machine &machine, bitmap_argb32 &dest, const rectangle &bounds, int state) override
 	{
 		auto font = machine.render().font_alloc("default");
-		draw_text(*font, dest, bounds, string_format("%0*d", m_digits, state).c_str(), m_textalign, color(state));
+		draw_text(*font, dest, bounds, string_format("%0*d", m_digits, state), m_textalign, color(state));
 	}
 
 private:
@@ -3137,18 +3137,13 @@ protected:
 					// allocate a temporary bitmap
 					bitmap_argb32 tempbitmap(dest.width(), dest.height());
 
-					const char *origs = m_stopnames[fruit].c_str();
-					const char *ends = origs + strlen(origs);
-					const char *s = origs;
-					char32_t schar;
-
 					// get the width of the string
 					float aspect = 1.0f;
 					s32 width;
 
 					while (1)
 					{
-						width = font->string_width(ourheight / num_shown, aspect, m_stopnames[fruit].c_str());
+						width = font->string_width(ourheight / num_shown, aspect, m_stopnames[fruit]);
 						if (width < bounds.width())
 							break;
 						aspect *= 0.9f;
@@ -3157,9 +3152,11 @@ protected:
 					s32 curx = bounds.left() + (bounds.width() - width) / 2;
 
 					// loop over characters
-					while (*s != 0)
+					std::string_view s = m_stopnames[fruit];
+					while (!s.empty())
 					{
-						int scharcount = uchar_from_utf8(&schar, s, ends - s);
+						char32_t schar;
+						int scharcount = uchar_from_utf8(&schar, &s[0], s.length());
 
 						if (scharcount == -1)
 							break;
@@ -3199,7 +3196,7 @@ protected:
 
 						// advance in the X direction
 						curx += font->char_width(ourheight/num_shown, aspect, schar);
-						s += scharcount;
+						s.remove_prefix(scharcount);
 					}
 				}
 			}
@@ -3294,7 +3291,7 @@ private:
 					s32 width;
 					while (1)
 					{
-						width = font->string_width(dest.height(), aspect, m_stopnames[fruit].c_str());
+						width = font->string_width(dest.height(), aspect, m_stopnames[fruit]);
 						if (width < bounds.width())
 							break;
 						aspect *= 0.9f;
@@ -3305,15 +3302,12 @@ private:
 					// allocate a temporary bitmap
 					bitmap_argb32 tempbitmap(dest.width(), dest.height());
 
-					const char *origs = m_stopnames[fruit].c_str();
-					const char *ends = origs + strlen(origs);
-					const char *s = origs;
-					char32_t schar;
-
 					// loop over characters
-					while (*s != 0)
+					std::string_view s = m_stopnames[fruit];
+					while (!s.empty())
 					{
-						int scharcount = uchar_from_utf8(&schar, s, ends - s);
+						char32_t schar;
+						int scharcount = uchar_from_utf8(&schar, &s[0], s.length());
 
 						if (scharcount == -1)
 							break;
@@ -3353,7 +3347,7 @@ private:
 
 						// advance in the X direction
 						curx += font->char_width(dest.height(), aspect, schar);
-						s += scharcount;
+						s.remove_prefix(scharcount);
 					}
 				}
 			}
@@ -3650,7 +3644,7 @@ void layout_element::component::draw_text(
 		render_font &font,
 		bitmap_argb32 &dest,
 		const rectangle &bounds,
-		const char *str,
+		std::string_view str,
 		int align,
 		const render_color &color)
 {
@@ -3696,15 +3690,10 @@ void layout_element::component::draw_text(
 	bitmap_argb32 tempbitmap(dest.width(), dest.height());
 
 	// loop over characters
-	const char *origs = str;
-	const char *ends = origs + strlen(origs);
-	const char *s = origs;
-	char32_t schar;
-
-	// loop over characters
-	while (*s != 0)
+	while (!str.empty())
 	{
-		int scharcount = uchar_from_utf8(&schar, s, ends - s);
+		char32_t schar;
+		int scharcount = uchar_from_utf8(&schar, &str[0], str.length());
 
 		if (scharcount == -1)
 			break;
@@ -3743,7 +3732,7 @@ void layout_element::component::draw_text(
 
 		// advance in the X direction
 		curx += font.char_width(bounds.height(), aspect, schar);
-		s += scharcount;
+		str.remove_prefix(scharcount);
 	}
 }
 

src/frontend/mame/cheat.cpp
@@ -212,11 +212,11 @@ void cheat_parameter::save(emu_file &cheatfile) const
 	{
 		// if no items, just output min/max/step
 		if (m_minval != 0)
-			cheatfile.printf(" min=\"%s\"", m_minval.format().c_str());
+			cheatfile.printf(" min=\"%s\"", m_minval.format());
 		if (m_maxval != 0)
-			cheatfile.printf(" max=\"%s\"", m_maxval.format().c_str());
+			cheatfile.printf(" max=\"%s\"", m_maxval.format());
 		if (m_stepval != 1)
-			cheatfile.printf(" step=\"%s\"", m_stepval.format().c_str());
+			cheatfile.printf(" step=\"%s\"", m_stepval.format());
 		cheatfile.printf("/>\n");
 	}
 	else
@@ -224,7 +224,7 @@ void cheat_parameter::save(emu_file &cheatfile) const
 		// iterate over items
 		cheatfile.printf(">\n");
 		for (item const &curitem : m_itemlist)
-			cheatfile.printf("\t\t\t<item value=\"%s\">%s</item>\n", curitem.value().format().c_str(), curitem.text());
+			cheatfile.printf("\t\t\t<item value=\"%s\">%s</item>\n", curitem.value().format(), curitem.text());
 		cheatfile.printf("\t\t</parameter>\n");
 	}
 }
@@ -533,15 +533,15 @@ void cheat_script::script_entry::save(emu_file &cheatfile) const
 		// output an action
 		cheatfile.printf("\t\t\t<action");
 		if (!m_condition.is_empty())
-			cheatfile.printf(" condition=\"%s\"", cheat_manager::quote_expression(m_condition).c_str());
-		cheatfile.printf(">%s</action>\n", cheat_manager::quote_expression(m_expression).c_str());
+			cheatfile.printf(" condition=\"%s\"", cheat_manager::quote_expression(m_condition));
+		cheatfile.printf(">%s</action>\n", cheat_manager::quote_expression(m_expression));
 	}
 	else
 	{
 		// output an output
 		cheatfile.printf("\t\t\t<output format=\"%s\"", m_format.c_str());
 		if (!m_condition.is_empty())
-			cheatfile.printf(" condition=\"%s\"", cheat_manager::quote_expression(m_condition).c_str());
+			cheatfile.printf(" condition=\"%s\"", cheat_manager::quote_expression(m_condition));
 
 		if (m_line != 0)
 			cheatfile.printf(" line=\"%d\"", m_line);
@@ -588,14 +588,14 @@ void cheat_script::script_entry::validate_format(std::string const &filename, in
 
 		// look for a valid type
 		if (!strchr("cdiouxX", *p))
-			throw emu_fatalerror("%s.xml(%d): invalid format specification \"%s\"\n", filename, line, m_format.c_str());
+			throw emu_fatalerror("%s.xml(%d): invalid format specification \"%s\"\n", filename, line, m_format);
 	}
 
 	// did we match?
 	if (argscounted < argsprovided)
-		throw emu_fatalerror("%s.xml(%d): too many arguments provided (%d) for format \"%s\"\n", filename, line, argsprovided, m_format.c_str());
+		throw emu_fatalerror("%s.xml(%d): too many arguments provided (%d) for format \"%s\"\n", filename, line, argsprovided, m_format);
 	if (argscounted > argsprovided)
-		throw emu_fatalerror("%s.xml(%d): not enough arguments provided (%d) for format \"%s\"\n", filename, line, argsprovided, m_format.c_str());
+		throw emu_fatalerror("%s.xml(%d): not enough arguments provided (%d) for format \"%s\"\n", filename, line, argsprovided, m_format);
 }
 
 
@@ -662,7 +662,7 @@ void cheat_script::script_entry::output_argument::save(emu_file &cheatfile) cons
 	cheatfile.printf("\t\t\t\t<argument");
 	if (m_count != 1)
 		cheatfile.printf(" count=\"%d\"", int(m_count));
-	cheatfile.printf(">%s</argument>\n", cheat_manager::quote_expression(m_expression).c_str());
+	cheatfile.printf(">%s</argument>\n", cheat_manager::quote_expression(m_expression));
 }
 
 
@@ -763,7 +763,7 @@ void cheat_entry::save(emu_file &cheatfile) const
 	bool const has_scripts(m_off_script || m_on_script || m_run_script || m_change_script);
 
 	// output the cheat tag
-	cheatfile.printf("\t<cheat desc=\"%s\"", m_description.c_str());
+	cheatfile.printf("\t<cheat desc=\"%s\"", m_description);
 	if (m_numtemp != DEFAULT_TEMP_VARIABLES)
 		cheatfile.printf(" tempvariables=\"%d\"", m_numtemp);
 
@@ -777,7 +777,7 @@ void cheat_entry::save(emu_file &cheatfile) const
 
 		// save the comment
 		if (!m_comment.empty())
-			cheatfile.printf("\t\t<comment><![CDATA[\n%s\n\t\t]]></comment>\n", m_comment.c_str());
+			cheatfile.printf("\t\t<comment><![CDATA[\n%s\n\t\t]]></comment>\n", m_comment);
 
 		// output the parameter, if present
 		if (m_parameter) m_parameter->save(cheatfile);
@@ -1235,7 +1235,7 @@ void cheat_manager::render_text(mame_ui_manager &mui, render_container &containe
 		if (!m_output[linenum].empty())
 		{
 			// output the text
-			mui.draw_text_full(container, m_output[linenum].c_str(),
+			mui.draw_text_full(container, m_output[linenum],
 					0.0f, float(linenum) * mui.get_line_height(), 1.0f,
 					m_justify[linenum], ui::text_layout::NEVER, mame_ui_manager::OPAQUE_,
 					rgb_t::white(), rgb_t::black(), nullptr, nullptr);

src/frontend/mame/ui/about.cpp
@@ -58,11 +58,11 @@ menu_about::~menu_about()
 void menu_about::populate(float &customtop, float &custombottom)
 {
 	std::string title = string_format(_("%1$s %2$s"), emulator_info::get_appname(), bare_build_version);
-	item_append(title, "", 0, nullptr);
+	item_append(title, 0, nullptr);
 	item_append(menu_item_type::SEPARATOR);
 
 	for (char const *const *line = copying_text; *line; ++line)
-		item_append(*line, "", 0, nullptr);
+		item_append(*line, 0, nullptr);
 
 	item_append(menu_item_type::SEPARATOR);
 }

src/frontend/mame/ui/analogipt.cpp
@@ -207,7 +207,7 @@ void menu_analog::populate(float &customtop, float &custombottom)
 					first_entry = false;
 				else
 					item_append(menu_item_type::SEPARATOR);
-				item_append(string_format("[root%s]", prev_owner->tag()), "", 0, nullptr);
+				item_append(string_format("[root%s]", prev_owner->tag()), 0, nullptr);
 			}
 		}
 

commit bb0e36e6305baa47e368062961f6c392ad5b277f

ui/menu.cpp: Fix highlighting glitch with input menu for inputs bound to O (regressed in a941526996d848c8baf36a044703c1cb76894647)

src/frontend/mame/ui/menu.cpp
@@ -701,13 +701,13 @@ void menu::draw(uint32_t flags)
 					}
 
 					// customize subitem text color
-					if (!core_strnicmp(&subitem_text[0], _("On"), subitem_text.length()))
+					if (!core_stricmp(pitem.subtext.c_str(), _("On")))
 						fgcolor2 = rgb_t(0x00,0xff,0x00);
 
-					if (!core_strnicmp(&subitem_text[0], _("Off"), subitem_text.length()))
+					if (!core_stricmp(pitem.subtext.c_str(), _("Off")))
 						fgcolor2 = rgb_t(0xff,0x00,0x00);
 
-					if (!core_strnicmp(&subitem_text[0], _("Auto"), subitem_text.length()))
+					if (!core_stricmp(pitem.subtext.c_str(), _("Auto")))
 						fgcolor2 = rgb_t(0xff,0xff,0x00);
 
 					// draw the subitem right-justified